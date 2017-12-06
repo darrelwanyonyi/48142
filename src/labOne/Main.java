103326320ef223c5a12f1d43df835a5f52d136d5
DarrelWanyonyi <darrel.sakwa@gmail.com>
2017-12-06 19:25:31 +0300

    Lab 1 initial commit

---
+++ src/labOne/Main.java
@@ -0,0 +1,21 @@
+package labOne;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //In scenario 1, catch the specific exception as indicated by the error
+
+        try {
+            String input = "is";
+            int user_input = Integer.parseInt(input);
+        }catch (NumberFormatException nfe){
+            System.out.println(nfe.getMessage());
+        }catch (Exception e) { //2.	In scenario 2, catch a general exception
+
+            System.out.println("Check the Number Format Error");
+        }
+
+    }
+
+}
